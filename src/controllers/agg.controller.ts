8be9b8414b03ba1bfb8cef0870daf934b92a9a5b
ngJosh <joshfelix@stantoninnovations.com>
2019-01-20 03:22:45 -0500

    added some basic scrape on wine.com

--- src/controllers/agg.controller.ts
+++ src/controllers/agg.controller.ts
@@ -1,8 +1,12 @@
 import { Get, Controller, Post } from '@nestjs/common';
 import { AggService } from '../services/agg.service';
 
-@Controller()
+import * as puppeteer from 'puppeteer';
+import { Result } from 'range-parser';
+
+@Controller('api/agg')
 export class AggController {
+    url: string;
     constructor(private readonly aggService: AggService) { }
 
     @Get()
@@ -11,4 +15,20 @@ export class AggController {
             Message: 'Hello Node js web api'
         }
     }
+
+    @Get('details')
+    async getWineDetails() {
+        this.url = 'https://www.wine.com/product/frias-family-vineyard-sauvignon-blanc-2014/184940'
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+        let result = await page.goto(this.url);
+
+        console.log('result', result);
+
+        await page.waitFor(2000);
+
+        browser.close();
+        return result.json;
+    }
+
 }
