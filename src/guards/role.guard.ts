e8406b01d8f09746a305cd753f539e680242f256
Joshua Felix <jfelix76@gmail.com>
2019-05-19 11:53:14 -0400

    Update role.guard.ts

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -12,12 +12,12 @@ export class RoleGuard implements CanActivate {
     }
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        // เก็บค่า Request
+        
         const request = context.switchToHttp().getRequest() as Request;
-        // ตรวจสอบว่ามี user login เข้ามาหรือไม่
+        
         if (request.user) {
             const userLogin = request.user as IAccount;
-            // ค้นหาว่า User login มี Role ตรงกับที่ กำหนดมาหรือป่าว
+            
             const searchRoles = this.roles.filter(role => role == userLogin.role);
             return searchRoles.length > 0;
         }
